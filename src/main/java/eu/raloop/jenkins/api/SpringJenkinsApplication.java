66229bb71353b47c16ca3b72ef9e3178d0b83b74
raloop <luprares@gmail.com>
2023-02-06 15:19:39 +0200

    modified pom file and startting file

--- src/main/java/eu/raloop/jenkins/api/SpringJenkinsApplication.java
+++ src/main/java/eu/raloop/jenkins/api/SpringJenkinsApplication.java
@@ -1,11 +1,13 @@
 package eu.raloop.jenkins.api;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
+
 
 
 @SpringBootApplication
